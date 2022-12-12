899bb9890e521a1b5b04a8bff66a828f5065d450
apoorva240 <117351441+apoorva240@users.noreply.github.com>
2022-12-12 14:41:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
         .controller('MyController', function ($scope, $http) {
             
-            $http.get('/data.json').then(function (response) {
+            $http.get('https://github.com/apoorva240/lab7/blob/main/data.json').then(function (response) {
                 $scope.datas = response.data;
             });
 
